6e387d912b1a118f1f589497c5b4672a956ed320
ManuA7X <44942727+ManuA7X@users.noreply.github.com>
2024-05-07 16:25:41 +0200

    metodo 1

--- PracticaArrays/src/util/Arrays.java
+++ PracticaArrays/src/util/Arrays.java
@@ -1,5 +1,28 @@
 package util;
 
 public abstract class Arrays {
-
+	
+	/**
+     * Calcula la media de un Array de numeros enteros entre el 0 y el 10.
+     *
+     * @param notas 						El Array de notas.
+     * @return 								La media de las notas.
+     * @throws IllegalArgumentException 	Si alguna nota no esta entre el 0 y el 10.
+     */
+	public static double mediaNotas(int [] notas) {
+		for (int nota : notas) {
+			if(nota < 0 || nota > 10) {
+				throw new IllegalArgumentException("Las notas deben estar entre el 0 y el 10.");
+			}
+		}
+		
+		int suma = 0;
+		for (int nota : notas) {
+			suma += nota;
+		}
+		return (double) suma / notas.length;
+		
+	}
+	
+	
 }
